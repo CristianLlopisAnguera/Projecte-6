478179dd430e26b6433e3fbd406f178aca051b54
CristianLlopisAnguera <cllopisanguera@gmail.com>
2025-04-24 10:23:24 +0200

    Update and rename P6_RA1_T1_A1_Hola_mon.py.py to P6_RA1_T1_A1_Hola_mon.py

---
+++ P6_RA1_T1_A1_Hola_mon.py
@@ -0,0 +1,10 @@
+#Administració de sistemes informatics en Xarxa
+#Autor: Cristian Llopis Anguera
+#Data: 24/04/2025
+
+#Versió:
+
+#Descripció(programa): Retorna el nom utilitzant un print.
+
+nom="CRISTIAN"
+print("El meu nom és:", nom)
