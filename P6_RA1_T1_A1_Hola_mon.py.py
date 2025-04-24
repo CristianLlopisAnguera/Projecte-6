cd9f016679ab43a7663f8220c8ae0f2b60e13873
CristianLlopisAnguera <cllopisanguera@gmail.com>
2025-04-24 09:57:43 +0200

    Add files via upload

---
+++ P6_RA1_T1_A1_Hola_mon.py.py
@@ -0,0 +1,27 @@
+#Administració de sistemes informatics en Xarxa
+#Autor: Cristian Llopis Anguera
+#Data:
+
+#Versió:
+
+#Descripció(programa):
+
+#
+
+#PRINT
+#-----------------------------
+print ("Hola mon!")
+
+a=20
+b=20
+print(a+b)
+
+nom="CRISTIAN"
+print("El meu nom és:", nom)
+#-----------------------------
+
+#INPUT
+#-----------------------------
+a=input()
+print(a)
+#-----------------------------
