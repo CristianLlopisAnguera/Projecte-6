aec8308e5160f193334c7c9fbff4e44c7122e193
CristianLlopisAnguera <cllopisanguera@gmail.com>
2025-04-24 10:27:07 +0200

    Add files via upload

---
+++ P6_RA1_T1_A1_nomedat.py
@@ -0,0 +1,10 @@
+#Administració de sistemes informatics en Xarxa
+#Autor: Cristian Llopis Anguera
+#Data: 24/04/2025
+
+#Versió:
+
+#Descripció(programa): Aquest programa retorna la meva edat amb un print.
+
+edat="19"
+print("La meva edat és:", edat)
